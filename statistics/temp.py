603640bbd3d66441c55891f7aea7f88cf26bfa23
amithapa <amithapa1994@gmail.com>
2023-09-14 23:16:47 +0530

    mediapipe library

---
+++ statistics/temp.py
@@ -0,0 +1,26 @@
+# create functions to add, subtract, multiply and divide numbers
+# This function adds two numbers
+def add(x: int, y: int) -> int:
+    # Add x to y
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x: int, y: int) -> int:
+    # Subtract y from x
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x: int, y: int) -> int:
+    # Multiply x by y
+    return x * y
+
+# This function divides two numbers
+def divide (x: int, y: int) -> float:
+    # Divide x by y
+    return x / y
+
+def main():
+    print("Hello world")
+
+if __name__ == "__main__":
+    main()
